46c048831649a2fccb011b34ee94695dd0b3378d
Eduard <martynenkoeduard1@gmail.com>
2023-09-23 18:32:44 +0300

    Simplified the Book class by replacing it with a record, which reduced the boilerplate code for constructors, getters, equals, hashCode, and toString methods

--- src/Book.java
+++ src/Book.java
@@ -1,21 +1,4 @@
-public class Book {
-    private String title;
-    private String author;
-    private String isbn;
-    private int yearOfPublication;
+public record Book(String title, String author, String isbn, int yearOfPublication) {
 
-
-public Book(String title,String author, String isbn, int yearOfPublication){
-    this.title = title;
-    this.author = author;
-    this.isbn = isbn;
-    this.yearOfPublication= yearOfPublication;
-}
-
-public String getTitle(){return this.title;}
-public String getAuthor(){return this.author;}
-public String getIsbn(){return this.isbn;}
-
-public int getYearOfPublication(){return this.yearOfPublication;}
 
 }
